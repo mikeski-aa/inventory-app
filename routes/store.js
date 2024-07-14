059b2f4f9ee88d829ba6e5903255aeb9ed33232e
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-07-14 11:14:30 +0200

    Controller skeleton finished

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Require controller modules
 const category_Controller = require("../controllers/categoryController");
-const item_Controller = require("../controllers/itemController");
+const item_controller = require("../controllers/itemController");
 
 // All ITEM ROUTES  ///
 
@@ -14,50 +14,50 @@ router.get("/", item_controller.index);
 router.get("/items", item_controller.item_list);
 
 // GET for one item
-router.get("/items/:id", item_controller.item);
+router.get("/items/:id", item_controller.item_item);
 
 // GET request for creating a new item
-router.get("/items/create", item_controller.create_get);
+router.get("/items/create", item_controller.item_create_get);
 
 // POST request for new item
-router.post("/items/create", item_controller.create_post);
+router.post("/items/create", item_controller.item_create_post);
 
 // GET request to delete an item
-router.get("/items/:id/delete", item_controller.delete_get);
+router.get("/items/:id/delete", item_controller.item_delete_get);
 
 // POST request to delete an item
-router.post("/items/:id/delete", item_controller.delete_post);
+router.post("/items/:id/delete", item_controller.item_delete_post);
 
 // GET request to update an item
-router.get("/items/:id/update", item_controller.update_get);
+router.get("/items/:id/update", item_controller.item_update_get);
 
 // POST request to update an item
-router.post("/items/:id/update", item_controller.update_post);
+router.post("/items/:id/update", item_controller.item_update_post);
 
 // ALL CATEGORY ROUTES //
 
 // GET all categories
-router.get("/categories", category_Controller.categories);
+router.get("/categories", category_Controller.category_list);
 
 // GET specific category
-router.get("/categories/:id", category_Controller.category);
+router.get("/categories/:id", category_Controller.category_category);
 
 // GET create new category
-router.get("/categories/create", category_Controller.create_get);
+router.get("/categories/create", category_Controller.category_create_get);
 
 // POST create new category
-router.post("/categories/create", category_Controller.create_post);
+router.post("/categories/create", category_Controller.category_create_post);
 
 // GET delete category
-router.get("/categories/:id/delete", category_Controller.delete_get);
+router.get("/categories/:id/delete", category_Controller.category_delete_get);
 
 // POST delete category
-router.post("/categories/:id/delete", category_Controller.delete_post);
+router.post("/categories/:id/delete", category_Controller.category_delete_post);
 
 // GET update category
-router.get("/categories/:id/update", category_Controller.update_get);
+router.get("/categories/:id/update", category_Controller.category_update_get);
 
 // POST update category
-router.post("/categories/:id/update", category_Controller.update_post);
+router.post("/categories/:id/update", category_Controller.category_update_post);
 
 module.exports = router;
